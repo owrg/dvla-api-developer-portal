7aa7acf18df4046e493cf22fffbb1df1d824291b
owrg <oliver.guildford@gmail.com>
2020-01-15 15:01:52 +0000

    Updated the fake email flows, added flows for test and prod and renamed files more logically

--- dvla-externaldevportal/app/routes.js
+++ dvla-externaldevportal/app/routes.js
@@ -9,14 +9,10 @@ router.use('/node_modules', express.static('node_modules'))
 router.post('/form-handler', function (req, res) {
   console.log(req.session.data['VESenvironment'])
   if (req.session.data['VESenvironment'] ==='Test'){
-/*    if (req.session.data['notifications-name'] ==='email'){
-      res.redirect('/v0/api/register-for-ves-email')
-    } else {
-*/
-      res.redirect('/v0/api/test-application-complete')
-    }
+      res.redirect('/v0/api/ves-application-complete-test');
+    
   } else {
-    res.redirect('/v0/api/register-for-ves-api-tacs')
+    res.redirect('/v0/api/ves-application-02')
   }
 })
 
