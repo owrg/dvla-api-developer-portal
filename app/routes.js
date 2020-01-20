f343b8061132c2bfcc2aaa1eedc7d92c508600ac
owrg <oliver.guildford@gmail.com>
2020-01-20 16:12:31 +0000

    Added the email confirmation step, removed the Test vs Prod to force the Test route

--- app/routes.js
+++ app/routes.js
@@ -7,13 +7,15 @@ router.use('/node_modules', express.static('node_modules'))
 
 
 router.post('/form-handler', function (req, res) {
+  res.redirect('/v0/api/ves-application-complete-test');    
+/*
   console.log(req.session.data['VESenvironment'])
   if (req.session.data['VESenvironment'] ==='Test'){
-      res.redirect('/v0/api/ves-application-complete-test');
-    
+      res.redirect('/v0/api/ves-application-complete-test');    
   } else {
     res.redirect('/v0/api/ves-application-02')
   }
+*/
 })
 
 
