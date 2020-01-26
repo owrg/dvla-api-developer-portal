df4794725ca5425bc65ac55ae28d50f55a02d554
owrg <oliver.guildford@gmail.com>
2020-01-26 23:05:08 +0000

    Created Step 1 Environment, then Step 2 form, then Step 3 Ts and Cs if Prod

--- app/routes.js
+++ app/routes.js
@@ -6,16 +6,15 @@ router.use('/node_modules', express.static('node_modules'))
 
 
 
-router.post('/form-handler', function (req, res) {
-  res.redirect('/v0/api/ves-application-complete-test');    
-/*
+router.post('/v0/form-handler', function (req, res) {
+
   console.log(req.session.data['VESenvironment'])
   if (req.session.data['VESenvironment'] ==='Test'){
-      res.redirect('/v0/api/ves-application-complete-test');    
+      res.redirect('/v0/api/ves-application-test');    
   } else {
-    res.redirect('/v0/api/ves-application-02')
+    res.redirect('/v0/api/ves-application-prod-01')
   }
-*/
+
 })
 
 
