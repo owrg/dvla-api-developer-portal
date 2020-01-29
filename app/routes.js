f8938873b1b0cccc4b4a7e2c809ac32ae3320b57
owrg <oliver.guildford@gmail.com>
2020-01-29 16:37:57 +0000

    Added multiple steps of questions to allow for Test API users to migrate to Prod

--- app/routes.js
+++ app/routes.js
@@ -12,11 +12,31 @@ router.get('/v0/', function (req, res) {
 
 router.post('/v0/form-handler', function (req, res) {
 
-  console.log(req.session.data['VESenvironment'])
-  if (req.session.data['VESenvironment'] ==='Test'){
-      res.redirect('/v0/api/ves-application-test');    
+//  console.log()
+
+////////////////////////////////////////////////////
+// STEP 1  
+  if (req.session.data['VESenvironment'] ==='Production'){
+
+////////////////////////////////////////////////////
+// STEP 2  
+    if (req.session.data['VESTestKey'] ==='TestKey'){
+////////////////////////////////////////////////////
+// STEP 3
+      if(req.session.data['ves-test-api-key'] === "key")
+        res.redirect('/v0/api/ves-application-prod-03-key-valid')
+      else if(req.session.data['ves-test-api-key'] === "invalid"){
+        res.redirect('/v0/api/ves-application-prod-03-key-invalid')
+      } else {
+        res.redirect('/v0/api/ves-application-prod-02-key')
+       }
+    } else if (req.session.data['VESTestKey'] ==='NoTestKey'){
+      res.redirect('/v0/api/ves-application-prod-02-nokey');    
+    } else {
+      res.redirect('/v0/api/ves-application-prod-01');    
+    }
   } else {
-    res.redirect('/v0/api/ves-application-prod-01')
+    res.redirect('/v0/api/ves-application-test')
   }
 
 })
